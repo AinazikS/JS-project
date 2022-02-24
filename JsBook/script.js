28b0448e5f08e974a59b17a538fce84da701f0c1
Ainazik <98087256+AinazikS@users.noreply.github.com>
2022-02-24 16:10:37 +0600

    Update script.js

--- JsBook/script.js
+++ JsBook/script.js
@@ -11,3 +11,100 @@ let CurrentUser = 'John';
 
 
 
+
+
+
+//3rd one
+let name = "Ilya";
+
+// выражение - число 1
+alert( `hello ${1}` ); // hello 1
+
+// выражение - строка "name"
+alert( `hello ${"name"}` ); // hello name
+
+// выражение - переменная, вставим её в строку
+alert( `hello ${name}` ); // hello Ilya
+
+
+
+
+
+//4th ONE
+<script>
+"use strict";
+
+let yourName = prompt("What is your name?");
+
+alert( yourName ); 
+</script>
+
+
+//
+let a = 1, b = 1;
+
+let c = ++a; // 2
+let d = b++; // 1
+// a = 2
+// b = 2
+// c = 2
+// d = 1
+
+let a = 2;
+
+let x = 1 + (a *= 2); // a = 4; x = 5;
+
+/*
+
+"" + 1 + 0 = "10" 
+"" - 1 + 0 = -1
+1 = true + false
+2 = 6 / "2"
+6 = "2" * "3"
+"9px" = 4 + 5 + "px" 
+"$45" = "$" + 4 + 5 
+2 = "4" - 2
+NaN  = "4px" - 2
+7 / 0 = Infinity
+"  -9  " + 5 = "  -9  5"
+-14 = "  -9  " - 5
+1 = null + 1 
+undefined + 1 = NaN 
+" \t \n" - 2 = -2 
+
+*/
+
+<script>
+"use strict";
+
+let a = prompt("Первое число?", 1);
+let b = prompt("Второе число?", +2);
+
+alert(+a + +b); // 12
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
