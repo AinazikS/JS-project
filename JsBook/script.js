abfb6f4e0669bebe6914728d5b71521c7d297b4a
Ainazik <98087256+AinazikS@users.noreply.github.com>
2022-02-26 08:05:58 +0600

    Update script.js

--- JsBook/script.js
+++ JsBook/script.js
@@ -323,6 +323,52 @@ switch (number) {
 
 
 
+// function checkAge(age) {return(age > 18)? true:confirm('Родители разрешили?');}
+// 
+// function checkAge(age) {return(age > 18) || confirm('Родители разрешили?');}
+
+// function min(a,b) {return (a > b)? b : a}
+
+
+
+
+// function pow(x,n) {return x ** n;}
+
+// let a = prompt("Enter 1st numbers: " , "");
+// let b = prompt("Enter 2nd numbers: " , "");
+
+// alert(pow(+a,+b));
+
+
+function pow(x, n) {
+  let result = x;
+
+  for (let i = 1; i < n; i++) {
+    result *= x;
+  }
+
+  return result;
+}
+
+let x = prompt("x?", '');
+let n = prompt("n?", '');
+
+if (n < 1) {
+  alert(`Степень ${n} не поддерживается, используйте натуральное число`);
+} else {
+  alert( pow(x, n) );
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
